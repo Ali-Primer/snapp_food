7d07ccdfae5f0bc877eae1fbfe65148f4146d25c
Ali Mousavi <mousavia465@gmail.com>
2024-02-03 23:56:33 +0330

    main header maded

---
+++ src/app/components/header.js
@@ -0,0 +1,54 @@
+import { FaLocationDot } from "react-icons/fa6";
+import { IoPersonOutline } from "react-icons/io5";
+import { MdNotes } from "react-icons/md";
+import { IoIosArrowDown } from "react-icons/io";
+import { IoSearch } from "react-icons/io5";
+import Image from "next/image";
+import logo from '../../../public/Logo/images.png'
+
+
+
+
+export const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <div className="header_main">
+                    <div className="main_container">
+                        <div className="main_right">
+                            <div className="right_logo">
+                                <Image  src={logo} width={50} quality={100} />
+                            </div>
+                            <div className="right_address">
+                                <FaLocationDot />
+                                <p className="address_text">بجنورد، خیابان لباف...</p>
+                                <IoIosArrowDown className="address_dropDown"/>
+                            </div>
+                        </div>
+                        <div className="main_search">
+                            <input />
+                            <IoSearch className="search_button"/>
+                        </div>
+                        <div className="main_left">
+                            <div className="left_account">
+                                <IoPersonOutline />
+                            </div>
+                            <div className="left_buys">
+                                <MdNotes />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div className="header_category">
+                    
+                </div>
+
+
+
+            </div>
+        </>
+    )
+}
