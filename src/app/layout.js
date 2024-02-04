45c24669946ac08d95d3a0a3d74c2993604ee253
Ali Mousavi <mousavia465@gmail.com>
2024-02-05 00:01:07 +0330

    footer made

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Footer } from "./components/footer";
 import { Header } from "./components/header";
 import "./globals.css";
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className={IranSansMobile.className}>
         <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
