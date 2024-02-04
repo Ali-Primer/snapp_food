449b0a6cc55eea2dfd95502b7aa510ab521ec1a8
Ali Mousavi <mousavia465@gmail.com>
2024-02-04 12:54:45 +0330

    category images added

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -5,11 +5,16 @@ import { IoIosArrowDown } from "react-icons/io";
 import { IoSearch } from "react-icons/io5";
 import Image from "next/image";
 import logo from '../../../public/Logo/images.png'
+import { categories } from "../db/category";
+import { HeaderCards } from "./headerCard";
 
 
 
 
 export const Header = () => {
+    
+    const CATEGORIES = categories
+
     return (
         <>
             <div className="header">
@@ -17,17 +22,17 @@ export const Header = () => {
                     <div className="main_container">
                         <div className="main_right">
                             <div className="right_logo">
-                                <Image  src={logo} width={50} quality={100} />
+                                <Image src={logo} width={50} quality={100} />
                             </div>
                             <div className="right_address">
                                 <FaLocationDot />
                                 <p className="address_text">بجنورد، خیابان لباف...</p>
-                                <IoIosArrowDown className="address_dropDown"/>
+                                <IoIosArrowDown className="address_dropDown" />
                             </div>
                         </div>
                         <div className="main_search">
                             <input />
-                            <IoSearch className="search_button"/>
+                            <IoSearch className="search_button" />
                         </div>
                         <div className="main_left">
                             <div className="left_account">
@@ -43,7 +48,9 @@ export const Header = () => {
 
 
                 <div className="header_category">
-                    
+                    {CATEGORIES.map(category => (
+                        <HeaderCards category={category}/>
+                    ))}
                 </div>
 
 
--- src/app/components/headerCard.js
+++ src/app/components/headerCard.js
@@ -1,11 +1,11 @@
 import Image from "next/image"
 
-export const headerCards = ({category}) => {
+export const HeaderCards = ({category}) => {
     return(
         <>
             <div className="header_card">
                 <div className="card_image">
-                    <Image  src={category.image} alt="" layout="fill"/> 
+                    <img  src={category.image} alt={category.name} layout="fill"/> 
                 </div>
                 <div className="card_text">
                     <p> {category.name}</p>
