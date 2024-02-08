68046c18b1fc121801d8d049ea25e24b8f43990e
Ali Mousavi <mousavia465@gmail.com>
2024-02-08 20:59:38 +0330

    home page done

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,64 +4,72 @@ import { IoLogoTwitter } from "react-icons/io";
 import { BiLogoTelegram } from "react-icons/bi";
 import { GrLinkedinOption } from "react-icons/gr";
 import { FaInstagram } from "react-icons/fa";
-
+import { TfiHeadphoneAlt } from "react-icons/tfi";
 
 
 
 export const Footer = () => {
     return (
-        <div className="footer">
-            <div className="footer_right">
-                <div className="right_info">
-                    <div className='info_img'>
-                        <Image src={logo} alt='لوگو اسنپ فود' quality={100} width={100} height={100} />
-                    </div>
-                    <div className='info_text'>
-                        <p className='text_title'>اسنپ فود</p>
-                        <p className='text_sum'>تجربه سفارش غذا از زود فود تا اسنپ فود</p>
-                    </div>
-                </div>
-                <div className="right_icons">
-                    <div className='icons_icon'>
-                        <IoLogoTwitter />
-                    </div>
-                    <div className='icons_icon'>
-                        <BiLogoTelegram />
-                    </div>
-                    <div className='icons_icon'>
-                        <GrLinkedinOption />
-                    </div>
-                    <div className='icons_icon'>
-                        <FaInstagram />
-                    </div>
-                </div>
-            </div>
+        <>
 
-            <div className="footer_middleRight">
-                <ul>
-                    <li>درباره اسنپ فود</li>
-                    <li>فرصت ها شغلی</li>
-                    <li>وبلاگ</li>
-                    <li>قوانین سایت</li>
-                    <li>حریم خصوصی</li>
-                    <li>ثبت نام فروشندگان</li>
-                </ul>
-            </div>
-            <div className="footer_middleLeft">
-                <ul>
-                    <li>تماس با اسنپ فود</li>
-                    <li>پرسش های متداول</li>
-                    <li>ثبت شکایات</li>
-                    <li>اپلیکیشن موبایل</li>
-                </ul>
-            </div>
-            <div className="footer_left">
-                <div className='left_images'>
-                    <div className='images_left'>
-                        <img src='https://snappfood.ir/static/images/senf.png' quality={100} />
+            <div className="footer">
+                <div className="footer_right">
+                    <div className="right_info">
+                        <div className='info_img'>
+                            <Image src={logo} alt='لوگو اسنپ فود' quality={100} width={100} height={100} />
+                        </div>
+                        <div className='info_text'>
+                            <p className='text_title'>اسنپ فود</p>
+                            <p className='text_sum'>تجربه سفارش غذا از زود فود تا اسنپ فود</p>
+                        </div>
+                    </div>
+                    <div className="right_icons">
+                        <div className='icons_icon'>
+                            <IoLogoTwitter />
+                        </div>
+                        <div className='icons_icon'>
+                            <BiLogoTelegram />
+                        </div>
+                        <div className='icons_icon'>
+                            <GrLinkedinOption />
+                        </div>
+                        <div className='icons_icon'>
+                            <FaInstagram />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="footer_middleRight">
+                    <ul>
+                        <li>درباره اسنپ فود</li>
+                        <li>فرصت ها شغلی</li>
+                        <li>وبلاگ</li>
+                        <li>قوانین سایت</li>
+                        <li>حریم خصوصی</li>
+                        <li>ثبت نام فروشندگان</li>
+                    </ul>
+                </div>
+                <div className="footer_middleLeft">
+                    <ul>
+                        <li>تماس با اسنپ فود</li>
+                        <li>پرسش های متداول</li>
+                        <li>ثبت شکایات</li>
+                        <li>اپلیکیشن موبایل</li>
+                    </ul>
+                </div>
+                <div className="footer_left">
+                    <div className='left_images'>
+                        <div className='images_left'>
+                            <img src='https://snappfood.ir/static/images/senf.png' quality={100} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div className='respond'>
+                <div className='respond_icon'>
+                    <TfiHeadphoneAlt />
+                </div>
+            </div>
+        </>
     )
 }
